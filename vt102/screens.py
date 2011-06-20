cb326f092ee72ee639142dc499ade3e59527b5d8
Sergei Lebedev <superbobry@gmail.com>
2011-06-20 15:20:33 +0400

    Moved `DiffScreen` back into `screen.py` and updated it to work with new cursor API

--- vt102/screens.py
+++ vt102/screens.py
@@ -773,3 +773,93 @@ class Screen(list):
                 replace = self.default_char._asdict()
 
         self.cursor.attrs = self.cursor.attrs._replace(**replace)
+
+
+class DiffScreen(Screen):
+    """A screen subclass, which maintains a set of dirty lines in its
+    :attr:`dirty` attribute. The end user is responsible for emptying
+    a set, when a diff is applied.
+
+    .. attribute:: dirty
+
+       A set of line numbers, which should be re-drawn.
+
+       >>> screen = DiffScreen(80, 24)
+       >>> screen.dirty.clear()
+       >>> screen.draw(u"!")
+       >>> screen.dirty
+       set([0])
+    """
+    def __init__(self, *args):
+        self.dirty = set()
+        super(DiffScreen, self).__init__(*args)
+
+    def set_mode(self, *modes, **kwargs):
+       	if mo.DECSCNM >> 5 in modes and kwargs.get("private"):
+            self.dirty.update(xrange(self.lines))
+        super(DiffScreen, self).set_mode(*modes, **kwargs)
+
+    def reset_mode(self, *modes, **kwargs):
+        if mo.DECSCNM >> 5 in modes and kwargs.get("private"):
+            self.dirty.update(xrange(self.lines))
+        super(DiffScreen, self).reset_mode(*modes, **kwargs)
+
+    def reset(self):
+        self.dirty.update(xrange(self.lines))
+        super(DiffScreen, self).reset()
+
+    def resize(self, *args, **kwargs):
+        self.dirty.update(xrange(self.lines))
+        super(DiffScreen, self).resize(*args, **kwargs)
+
+    def draw(self, *args):
+        self.dirty.add(self.cursor.y)
+        super(DiffScreen, self).draw(*args)
+
+    def index(self):
+        if self.cursor.y == self.margins.bottom:
+            self.dirty.update(xrange(self.lines))
+
+        super(DiffScreen, self).index()
+
+    def reverse_index(self):
+        if self.cursor.y == self.margins.top:
+            self.dirty.update(xrange(self.lines))
+
+        super(DiffScreen, self).reverse_index()
+
+    def insert_lines(self, *args):
+        self.dirty.update(xrange(self.cursor.y, self.lines))
+        super(DiffScreen, self).insert_lines(*args)
+
+    def delete_lines(self, *args):
+        self.dirty.update(xrange(self.cursor.y, self.lines))
+        super(DiffScreen, self).delete_lines(*args)
+
+    def insert_characters(self, *args):
+        self.dirty.add(self.cursor.y)
+        super(DiffScreen, self).insert_characters(*args)
+
+    def delete_characters(self, *args):
+        self.dirty.add(self.cursor.y)
+        super(DiffScreen, self).delete_characters(*args)
+
+    def erase_characters(self, *args):
+        self.dirty.add(self.cursor.y)
+        super(DiffScreen, self).erase_characters(*args)
+
+    def erase_in_line(self, *args):
+        self.dirty.add(self.cursor.y)
+        super(DiffScreen, self).erase_in_line(*args)
+
+    def erase_in_display(self, type_of=0):
+        self.dirty.update((
+            xrange(self.cursor.y + 1, self.lines),
+            xrange(0, self.cursor.y),
+            xrange(0, self.lines)
+        )[type_of])
+        super(DiffScreen, self).erase_in_display(type_of)
+
+    def alignment_display(self):
+        self.dirty.update(xrange(self.cursor.y, self.lines))
+        super(DiffScreen, self).alignment_display()
